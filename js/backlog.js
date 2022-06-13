b68390ebb154b329ea1f85515ccc668020d09cb6
Benjamin <benschuetze@gmx.de>
2022-06-13 14:53:41 +0200

    marker funcitonality re-added

--- js/backlog.js
+++ js/backlog.js
@@ -12,17 +12,36 @@ async function initBacklog() {
 function showTasksInBacklog() {
     for(i = 0; i < storedTasks.length; i++) {
         document.getElementById('backlog-container').innerHTML += tasksInBacklogHTML(i);
+        setMarkerColor(i);
     }
     
 }
 
-function tasksInBacklogHTML() {
+function tasksInBacklogHTML(i) {
     return `
     <div class="task-container">
+        <div  id="${i}" class="urgency-marker"></div>
         <p class="title-width margin-auto">${storedTasks[i]['title']}</p>
         <p class="category-width margin-auto">${storedTasks[i]['category']}</p>
         <p class="details-width margin-auto">${storedTasks[i]['description']}</p>
     </div>`;
+}
+
+function setMarkerColor(i) {
+    let marker = document.getElementById(i);
+    let task = storedTasks[i];
+
+    if(task['urgency'] == 'HIGH') {
+        marker.style.backgroundColor = 'red';
+    }
+
+    if(task['urgency'] == 'MEDIUM') {
+        marker.style.backgroundColor = 'yellow';
+    }
+
+    if(task['urgency'] == 'LOW') {
+        marker.style.backgroundColor = 'green';
+    }
 }
 
 
