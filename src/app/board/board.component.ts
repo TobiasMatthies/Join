7b813269bb3df39aa6b72398818ffdef318d9bdb
Tobias Matthies <tobiasmatthies@MacBook-Air-von-Tobias.local>
2022-11-29 11:20:47 -0300

    implemented drag and drop with status change

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
 import { AppStateService } from '../app-state/app-state.service';
 
 @Component({
@@ -7,14 +10,37 @@ import { AppStateService } from '../app-state/app-state.service';
   styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
-  currentDraggedElement: Task;
 
   constructor(public appState: AppStateService) {}
 
   ngOnInit(): void {}
 
-  changeStatus(status: string) {
-    this.currentDraggedElement['status'] = status;
+  changeStatus(event: CdkDragDrop<any>, status: string) {
+    let draggedTask;
+    draggedTask = this.appState.tasks.find(
+      (task) => task.id == +event.item.element.nativeElement.id
+    );
+
+    draggedTask['status'] = status;
     console.log('dropped');
+  }
+
+  onDrop(event: CdkDragDrop<any>, status: string) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      this.changeStatus(event, status);
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
