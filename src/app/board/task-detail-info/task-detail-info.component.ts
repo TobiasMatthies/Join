2337ddcd8deb88665612b0b19535c7f396ec333a
TobiasMatthies <tbsmatthies@gmail.com>
2026-01-09 13:00:35 -0300

    distinguishing assignees between members and externs

--- src/app/board/task-detail-info/task-detail-info.component.ts
+++ src/app/board/task-detail-info/task-detail-info.component.ts
@@ -1,25 +1,32 @@
+import { DatePipe, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { TaskDetailService } from '../../services/task-detail.service';
-import { ConfirmService } from './confirm.service';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { ButtonPrimaryComponent } from '../../customComponents/button-primary/button-primary.component';
-import { NgClass, NgStyle, DatePipe } from '@angular/common';
+import { Contact } from '../../models/contact.model';
+import { AppStateService } from '../../services/app-state.service';
+import { TaskDetailService } from '../../services/task-detail.service';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { ConfirmService } from './confirm.service';
 
 @Component({
-    selector: 'app-task-detail-info',
-    templateUrl: './task-detail-info.component.html',
-    styleUrls: ['./task-detail-info.component.css'],
-    imports: [
+  selector: 'app-task-detail-info',
+  templateUrl: './task-detail-info.component.html',
+  styleUrls: ['./task-detail-info.component.css'],
+  imports: [
     NgClass,
     NgStyle,
     ButtonPrimaryComponent,
     ConfirmDialogComponent,
-    DatePipe
-]
+    DatePipe,
+  ],
 })
 export class TaskDetailInfoComponent {
   constructor(
     public taskDetailService: TaskDetailService,
-    public confirmService: ConfirmService
+    public confirmService: ConfirmService,
+    private appState: AppStateService,
   ) {}
+
+  identifyContact(contact: Contact): Boolean {
+    return !!this.appState.contacts.find((c) => c.email === contact.email);
+  }
 }
